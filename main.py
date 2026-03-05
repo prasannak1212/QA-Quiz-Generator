8a0c4bf6650d5ee010525c0c5f522d65eaa90474
Prasanna K <prasanna12uo@gmail.com>
2026-03-06 04:11:33 +0530

    Adding comments

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from chains import qa_chain, quiz_chain
 
+# -----------------------------
+# Creating FastAPI instance
+# -----------------------------
 app = FastAPI()
 
-
+# -----------------------------
+# Pydantic Models
+# -----------------------------
 class QARequest(BaseModel):
     paragraph: str
     question: str
@@ -14,6 +19,9 @@ class QuizRequest(BaseModel):
     paragraph: str
 
 
+# -----------------------------
+# Creating QA endpoint
+# -----------------------------
 @app.post("/ask")
 def ask_question(data: QARequest):
     response = qa_chain(
@@ -23,6 +31,9 @@ def ask_question(data: QARequest):
     return {"answer": response}
 
 
+# -----------------------------
+# Creating Quiz endpoint
+# -----------------------------
 @app.post("/generate-quiz")
 def generate_quiz(data: QuizRequest):
     response = quiz_chain(
